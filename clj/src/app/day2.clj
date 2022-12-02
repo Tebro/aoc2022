022169699c1800578d401d078305e901f0ef5a48
Weber Richard <richard.weber@insta.fi>
2022-12-02 12:22:31 +0000

    Bench improvements for clj

--- clj/src/app/day2.clj
+++ clj/src/app/day2.clj
@@ -1,9 +1,11 @@
 (ns app.day2 (:require [clojure.string :as str]
-                       [clojure.java.io :as io]))
+                       [clojure.java.io :as io]
+                       [criterium.core :as crit]))
 
 (def opts1
   (let [loss 0 draw 3 win 6 rock 1 paper 2 scissor 3]
-    {"A X" (+ rock draw)
+    {0 0
+     "A X" (+ rock draw)
      "A Y" (+ paper win)
      "A Z" (+ scissor loss)
 
@@ -17,7 +19,8 @@
 
 (def opts2 
   (let [loss 0 draw 3 win 6 rock 1 paper 2 scissor 3]
-    {"A Y" (+ rock draw)
+    {0 0
+     "A Y" (+ rock draw)
      "A Z" (+ paper win)
      "A X" (+ scissor loss)
 
@@ -33,13 +36,24 @@
   (let [line-scores (map rules lines)]
     (reduce + line-scores)))
 
+(defn alg2 [rules lines]
+    (reduce (fn [a b] (+ a (rules b))) 0 lines))
+
 (comment
 
   (let [input (slurp (io/resource "day_2"))
         lines (str/split-lines input)]
-    {:part1 (time (alg opts1 lines))
-     :part2 (time (alg opts2 lines))})
+    {:part1 (alg opts1 lines)
+     :part2 (alg opts2 lines)})
+ 
+  (let [input (slurp (io/resource "day_2"))
+        lines (str/split-lines input)]
+    (crit/quick-bench (alg opts1 lines))
+    (crit/quick-bench (alg opts2 lines)))
 
+  (let [input (slurp (io/resource "day_2"))
+        lines (str/split-lines input)]
+    (crit/quick-bench (alg2 opts1 lines)))
 
 
 
