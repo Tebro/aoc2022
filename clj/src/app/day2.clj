3e7fb4710bb422f3172e77dd877a1fd933f936c8
Weber Richard <richard.weber@insta.fi>
2022-12-02 10:32:05 +0000

    Make clj impl better

--- clj/src/app/day2.clj
+++ clj/src/app/day2.clj
@@ -1,7 +1,7 @@
 (ns app.day2 (:require [clojure.string :as str]
                        [clojure.java.io :as io]))
 
-(defn opts [] 
+(def opts1
   (let [loss 0 draw 3 win 6 rock 1 paper 2 scissor 3]
     {"A X" (+ rock draw)
      "A Y" (+ paper win)
@@ -15,7 +15,7 @@
      "C Y" (+ paper loss)
      "C Z" (+ scissor draw)}))
 
-(defn opts2 [] 
+(def opts2 
   (let [loss 0 draw 3 win 6 rock 1 paper 2 scissor 3]
     {"A Y" (+ rock draw)
      "A Z" (+ paper win)
@@ -29,21 +29,18 @@
      "C X" (+ paper loss)
      "C Y" (+ scissor draw)}))
 
-(defn day2 []
-  (let [scores (opts)
-        scores2 (opts2)
-        input (slurp (io/resource "day_2"))
-        lines (str/split-lines input)
-        line-scores (map #(hash-map :score1 (scores %) :score2 (scores2 %)) lines)]
-    {:first (reduce + (map :score1 line-scores))
-     :second (reduce + (map :score2 line-scores))}))
+(defn alg [rules lines]
+  (let [line-scores (map #(rules %) lines)]
+    (reduce + line-scores)))
 
 (comment
-  
-  (day2)
 
-  (time (day2))
-  
+  (let [input (slurp (io/resource "day_2"))
+        lines (str/split-lines input)]
+    {:part1 (time (alg opts1 lines))
+     :part2 (time (alg opts2 lines))})
+
+
 
 
   )
