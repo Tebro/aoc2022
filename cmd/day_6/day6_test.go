28260a3c42bed69c579fa2ab7b14a4c54d367431
Weber Richard <richard.weber@insta.fi>
2022-12-07 10:05:39 +0000

    Benchmarks

--- cmd/day_6/day6_test.go
+++ cmd/day_6/day6_test.go
@@ -24,3 +24,21 @@ func Test_runpart2(t *testing.T) {
 	t.Fail()
 
 }
+
+func Benchmark_part1(b *testing.B) {
+	lines := strings.Split(data, "\n")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		part1(lines)
+	}
+}
+
+func Benchmark_part2(b *testing.B) {
+	lines := strings.Split(data, "\n")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		part2(lines)
+	}
+}
